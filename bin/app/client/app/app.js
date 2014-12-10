392f39a1b87e5befa33cfb8ce99125098f2684eb
crc442 <EequalsMCsquare91@gmail.com>
2014-12-09 20:30:08 -0500

    all setup

---
+++ bin/app/client/app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('appApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
